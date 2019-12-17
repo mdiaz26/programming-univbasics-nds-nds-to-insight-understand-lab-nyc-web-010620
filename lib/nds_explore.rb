c14af7c46935ef9e2af1c1644e7ce117a4bfc7fc
Michael Diaz <mike.diaz006@gmail.com>
2019-12-17 15:16:18 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,10 +8,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  counter = 0 
+  counter = 0
+  new_array = []
   while counter < directors_database[0][:movies].length
     # we want directors_database[0][1 (which should be the hash of :movies)][0 or :title (but we want this to loop through)]
-    pp "-#{directors_database[0][:movies][counter][:title]}"
+    new_array << directors_database[0][:movies][counter][:title]
     counter += 1
   end
+pp new_array
 end
