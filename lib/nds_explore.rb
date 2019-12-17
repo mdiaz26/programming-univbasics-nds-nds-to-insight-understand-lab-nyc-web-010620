eb4646e34536dddd0b41f43f53e4dfc04a7a1a4e
Michael Diaz <mike.diaz006@gmail.com>
2019-12-17 15:43:34 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,14 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp directors_database
 end
 
 def print_first_directors_movie_titles
+  counter = 0
+  while counter < directors_database[0][:movies].length
+    # we want directors_database[0][1 (which should be the hash of :movies)][0 or :title (but we want this to loop through)]
+    puts directors_database[0][:movies][counter][:title]
+    counter += 1
+  end
 end
