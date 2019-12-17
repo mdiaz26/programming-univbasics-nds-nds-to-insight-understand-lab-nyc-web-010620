49dc8091c34bda70959cfe4f4b230a74a57a136f
Michael Diaz <mike.diaz006@gmail.com>
2019-12-17 15:13:32 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ end
 
 def print_first_directors_movie_titles
   counter = 0 
-  while counter < directors_database.length
+  while counter < directors_database[0][:movies].length
     # we want directors_database[0][1 (which should be the hash of :movies)][0 or :title (but we want this to loop through)]
     pp directors_database[0][:movies][counter][:title]
     counter += 1
