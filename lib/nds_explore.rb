4da1529f2170a58d9c707e0f624e54e23ac78cfb
Michael Diaz <mike.diaz006@gmail.com>
2019-12-17 15:04:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 def print_first_directors_movie_titles
   counter = 0 
   while counter < directors_database.length
-    pp #{directors_database[counter]}
+    pp "#{directors_database[counter]}"
     counter += 1
   end
 end
